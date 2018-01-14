0b8de0c59172c8c2d0de49c2a4da6104e71c97c1
keyun <keyun@4417.com>
2018-01-14 20:55:18 +0800

    GO微信SDK-参考github.com/silenceper/wechat

---
+++ custom/custom.go
@@ -0,0 +1,67 @@
+package custom
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/silenceper/wechat/context"
+	"github.com/silenceper/wechat/util"
+)
+
+const (
+	customSendURL = "https://api.weixin.qq.com/cgi-bin/message/custom/send"
+)
+
+//Custom 客服消息
+type Custom struct {
+	*context.Context
+}
+
+//NewCustom 实例化
+func NewCustom(context *context.Context) *Custom {
+	ct := new(Custom)
+	ct.Context = context
+	return ct
+}
+
+//图片消息
+type CustomImageData struct {
+	ToUser  string `json:"touser"`
+	MsgType string `json:"msgtype"`
+	Image   struct {
+		MediaId string `json:"media_id"`
+	} `json:"image"`
+}
+
+type resCustomSend struct {
+	util.CommonError
+}
+
+//Send 发送客服消息
+func (custom *Custom) Send(customImage *CustomImageData) (err error) {
+	accessToken, err := custom.GetAccessToken()
+	if err != nil {
+		return
+	}
+	uri := fmt.Sprintf("%s?access_token=%s", customSendURL, accessToken)
+	response, err := util.PostJSON(uri, customImage)
+
+	var result resCustomSend
+	err = json.Unmarshal(response, &result)
+	if err != nil {
+		return
+	}
+
+	if result.ErrCode != 0 {
+		err = fmt.Errorf("create qrcode error : errcode=%v , errmsg=%v", result.ErrCode, result.ErrMsg)
+		return
+	}
+	return
+}
+
+//SetCustomImageData
+func (customImage *CustomImageData) SetCustomImageData(touser string, msgtype string, media_id string) {
+	customImage.ToUser = touser
+	customImage.MsgType = msgtype
+	customImage.Image.MediaId = media_id
+}
---
+++ qrcode/qrcode.go
@@ -0,0 +1,140 @@
+package qrcode
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/silenceper/wechat/context"
+	"github.com/silenceper/wechat/util"
+)
+
+const (
+	qrcodeSendURL = "https://api.weixin.qq.com/cgi-bin/qrcode/create"
+)
+
+//Qrcode 带参数二维码
+type Qrcode struct {
+	*context.Context
+}
+
+//NewQrcode 实例化
+func NewQrcode(context *context.Context) *Qrcode {
+	qr := new(Qrcode)
+	qr.Context = context
+	return qr
+}
+
+//整型参数
+type IDScene struct {
+	Scene_id int32 `json:"scene_id"`
+}
+
+//字符串参数
+type StrSecne struct {
+	Scene_str string `json:"scene_str"`
+}
+
+//临时IDqrcode 数据
+type IDQrcodeTemp struct {
+	ExpireSeconds int32  `json:"expire_seconds"` // 必须, 该二维码有效时间，以秒为单位。 最大不超过2592000（即30天）
+	ActionName    string `json:"action_name"`    // 必须, 二维码类型，QR_SCENE为临时的整型参数值，QR_STR_SCENE为临时的字符串参数值，QR_LIMIT_SCENE为永久的整型参数值，QR_LIMIT_STR_SCENE为永久的字符串参数值
+
+	ActionInfo struct {
+		Scene IDScene
+	} `json:"action_info"` //必须，
+}
+
+//临时Strqrcode 数据
+type StrQrcodeTemp struct {
+	ExpireSeconds int32  `json:"expire_seconds"` // 必须, 该二维码有效时间，以秒为单位。 最大不超过2592000（即30天）
+	ActionName    string `json:"action_name"`    // 必须, 二维码类型，QR_SCENE为临时的整型参数值，QR_STR_SCENE为临时的字符串参数值，QR_LIMIT_SCENE为永久的整型参数值，QR_LIMIT_STR_SCENE为永久的字符串参数值
+
+	ActionInfo struct {
+		Scene StrSecne `json:"scene"`
+	} `json:"action_info"` //必须，
+}
+
+//永久IDqrcode 数据
+type IDQrcode struct {
+	ActionName string `json:"action_name"` // 必须, 二维码类型，QR_SCENE为临时的整型参数值，QR_STR_SCENE为临时的字符串参数值，QR_LIMIT_SCENE为永久的整型参数值，QR_LIMIT_STR_SCENE为永久的字符串参数值
+
+	ActionInfo struct {
+		Scene IDScene
+	} `json:"action_info"` //必须，
+}
+
+//永久Strqrcode 数据
+type StrQrcode struct {
+	ActionName string `json:"action_name"` // 必须, 二维码类型，QR_SCENE为临时的整型参数值，QR_STR_SCENE为临时的字符串参数值，QR_LIMIT_SCENE为永久的整型参数值，QR_LIMIT_STR_SCENE为永久的字符串参数值
+	ActionInfo struct {
+		Scene StrSecne `json:"scene"`
+	} `json:"action_info"` //必须，
+}
+
+type resQrcodeSend struct {
+	util.CommonError
+	Ticket        string `json:"ticket"`
+	ExpireSeconds int32  `json:"expire_seconds"`
+	URL           string `json:"url"`
+}
+
+//Send 发送生成临时二维码请求
+func (qrcode *Qrcode) Send(qrcodeTemp *StrQrcodeTemp) (ticket string, err error) {
+	accessToken, err := qrcode.GetAccessToken()
+	if err != nil {
+		return
+	}
+	uri := fmt.Sprintf("%s?access_token=%s", qrcodeSendURL, accessToken)
+	response, err := util.PostJSON(uri, qrcodeTemp)
+
+	var result resQrcodeSend
+	err = json.Unmarshal(response, &result)
+	if err != nil {
+		return
+	}
+
+	if result.ErrCode != 0 {
+		err = fmt.Errorf("create qrcode error : errcode=%v , errmsg=%v", result.ErrCode, result.ErrMsg)
+		return
+	}
+
+	ticket = result.Ticket
+	return
+}
+
+//SetQrData
+func (qrcode *StrQrcodeTemp) SetQrData(expireSeconds int32, actionName string, actionStr string) {
+	qrcode.ExpireSeconds = expireSeconds
+	qrcode.ActionName = actionName
+	qrcode.ActionInfo.Scene.Scene_str = actionStr
+}
+
+//Send 发送生成永久二维码请求
+func (qrcode *Qrcode) SendForever(strQrcode *StrQrcode) (ticket string, err error) {
+	accessToken, err := qrcode.GetAccessToken()
+	if err != nil {
+		return
+	}
+	uri := fmt.Sprintf("%s?access_token=%s", qrcodeSendURL, accessToken)
+	response, err := util.PostJSON(uri, strQrcode)
+
+	var result resQrcodeSend
+	err = json.Unmarshal(response, &result)
+	if err != nil {
+		return
+	}
+
+	if result.ErrCode != 0 {
+		err = fmt.Errorf("create qrcode error : errcode=%v , errmsg=%v", result.ErrCode, result.ErrMsg)
+		return
+	}
+
+	ticket = result.Ticket
+	return
+}
+
+//SetForeverQrData
+func (qrcode *StrQrcode) SetForeverQrData(actionName string, actionStr string) {
+	qrcode.ActionName = actionName
+	qrcode.ActionInfo.Scene.Scene_str = actionStr
+}
